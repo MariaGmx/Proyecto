b7d76e31b480f61ea3adb3eafc283097f6a14e6b
MariaGmx <maritagomez3000@gmail.com>
2026-05-19 22:08:10 -0500

    Subiendo el archivo WelcomePanel

--- src/main/java/view/WelcomePanel.java
+++ src/main/java/view/WelcomePanel.java
@@ -165,6 +165,7 @@ public class WelcomePanel extends JFrame {
             timer.start();
         }
 
+        //MODIFICACION
         @Override
         protected void paintComponent(Graphics g) {
             Graphics2D g2d = (Graphics2D) g.create();
